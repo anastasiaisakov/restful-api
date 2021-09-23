d4dee1b3ff4712435ddc9c12716374ce7803bd4a
Anastasija Barbara Isakov <AnastasijaBarbara.Isakov@p3-group.com>
2021-09-23 14:43:31 +0200

    fixed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 from resources.item import Item, ItemList
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -13,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "jose"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>') # instead of the decorator
@@ -26,5 +21,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__': # so it doesn't run when importing app
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
